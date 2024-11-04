ec563878c4ebb1396afb2c321c44ac023f4ddc2c
salvoparisi <psalvo065@gmail.com>
2024-11-04 11:41:20 +0100

    add id

--- data/postsData.js
+++ data/postsData.js
@@ -1,29 +1,34 @@
 module.exports = [
     {
+      id: 1,
       title: "Post 1",
       content: "Contenuto del primo post.",
       image: "/images/post1.jpg",
       tags: ["Node.js", "Express", "JavaScript"]
     },
     {
+      id: 2,
       title: "Post 2",
       content: "Contenuto del secondo post.",
       image: "/images/post2.jpg",
       tags: ["API", "REST"]
     },
     {
+      id: 3,
       title: "Post 3",
       content: "Contenuto del terzo post.",
       image: "/images/post3.jpg",
       tags: ["Frontend", "Backend"]
     },
     {
+      id: 4,
       title: "Post 4",
       content: "Contenuto del quarto post.",
       image: "/images/post4.jpg",
       tags: ["Fullstack", "Programming"]
     },
     {
+      id: 5,
       title: "Post 5",
       content: "Contenuto del quinto post.",
       image: "/images/post5.jpg",
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const postsData = require('./data/postsData.js')
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -15,3 +16,17 @@ app.get('/posts', postsController.index);
 app.listen(port, () => {
   console.log(`listening http://localhost:${port}`);
 });
+
+app.get('/posts/:id', (req, res) =>{
+  const posts = postsData.find(pizza=>pizza.id === Number(req.params.id))
+  console.log(posts);
+  if(!posts){
+    return res.status(404).json({
+      error: "Not Found"
+    })
+  }
+  return res.json({
+    data: posts
+  })
+  
+})
