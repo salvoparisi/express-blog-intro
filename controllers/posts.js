7d64be807d01aceb3515d1da7558f887d44f630e
salvoparisi <psalvo065@gmail.com>
2024-11-04 11:47:34 +0100

    add controllers

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,27 @@
-const posts = require('../data/postsData');
+const postsData = require('../data/postsData');
 
-exports.index = (req, res) => {
+index = (req, res) => {
   res.json({
     count: posts.length,
     posts: posts
   });
 };
+
+show = (req, res) =>{
+  const posts = postsData.find(pizza=>pizza.id === Number(req.params.id))
+  console.log(posts);
+  if(!posts){
+    return res.status(404).json({
+      error: "Not Found"
+    })
+  }
+  return res.json({
+    data: posts
+  })
+  
+}
+
+module.exports={
+  index,
+  show
+}
--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-const postsData = require('./data/postsData.js')
 const express = require('express');
 const app = express();
 const port = 3000;
-
 const postsController = require('./controllers/posts');
 
 app.use(express.static('public'));
@@ -11,22 +9,10 @@ app.get('/', (req, res) => {
   res.send('<h1>Benvenuto nel mio blog!</h1>');
 });
 
-app.get('/posts', postsController.index);
-
 app.listen(port, () => {
   console.log(`listening http://localhost:${port}`);
 });
 
-app.get('/posts/:id', (req, res) =>{
-  const posts = postsData.find(pizza=>pizza.id === Number(req.params.id))
-  console.log(posts);
-  if(!posts){
-    return res.status(404).json({
-      error: "Not Found"
-    })
-  }
-  return res.json({
-    data: posts
-  })
-  
-})
+app.get('/posts', postsController.index);
+
+app.get('/posts/:id', postsController.show);
